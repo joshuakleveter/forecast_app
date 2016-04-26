6cfc2ae3374e8850df59f8fa7d12b482793fce55
Joshua Kleveter <joshua.kleveter@gmail.com>
2016-04-26 07:41:13 -0500

    Reformatted main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-'use strict';
+/////////////
+// Imports //
+/////////////
 
 const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 
+
+
+/////////
+// App //
+/////////
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -17,6 +26,7 @@ app.on('window-all-closed', function() {
     }
 });
 
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
@@ -25,9 +35,6 @@ app.on('ready', function() {
 
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
